df80add74f813bfa90ef9ee6a355f90da4d33b22
318400 <91246976+318400@users.noreply.github.com>
2021-09-30 16:32:35 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,57 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+let Y = 0
+let X = 0
+Y += 1
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.plotBrightness(X, Y, 33)
+        basic.pause(20)
+    }
+}
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.plotBrightness(Y, X, 132)
+        basic.pause(20)
+    }
+}
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.plotBrightness(X, Y, 193)
+        basic.pause(20)
+    }
+}
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.plot(Y, X)
+        basic.pause(20)
+    }
+}
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.plotBrightness(X, Y, 206)
+        basic.pause(20)
+    }
+}
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.plotBrightness(Y, X, 111)
+        basic.pause(50)
+    }
+}
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.plotBrightness(X, Y, 41)
+        basic.pause(20)
+    }
+}
+for (let Y = 0; Y <= 4; Y++) {
+    for (let X = 0; X <= 4; X++) {
+        led.unplot(Y, X)
+        basic.pause(20)
+    }
+}
+basic.forever(function () {
+	
+})
