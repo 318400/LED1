98fea9305017fe6dd1c69bc6ee4bf941fdb5c52d
318400 <91246976+318400@users.noreply.github.com>
2021-09-30 21:34:52 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
-let X = 0
-let Y = 0
 input.onButtonPressed(Button.A, function () {
-    X = 0
-    Y += 1
+    basic.clearScreen()
     for (let Y = 0; Y <= 4; Y++) {
         for (let X = 0; X <= 4; X++) {
             led.plotBrightness(X, Y, 33)
@@ -52,6 +49,49 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    for (let index2 = 0; index2 < 100; index2++) {
+        _2 = 0
+        if (_1 >= 4) {
+            index = -1
+        } else if (_1 <= 0) {
+            index = 1
+        }
+        if (_3 >= 4) {
+            _4 = -1
+        } else if (_3 <= 0) {
+            _4 = 1
+        }
+        if (_5 >= 4) {
+            _6 = -1
+        } else if (_5 <= 0) {
+            _6 = 1
+        }
+        _1 += index
+        _3 += _4
+        _5 += _6
+        for (let index2 = 0; index2 < 5; index2++) {
+            led.plotBrightness(_5, _2, 20)
+            led.plotBrightness(_3, _2, 60)
+            led.plot(_1, _2)
+            _2 += 1
+        }
+        basic.pause(100)
+    }
 })
+let index = 0
+let _6 = 0
+let _5 = 0
+let _4 = 0
+let _3 = 0
+let _2 = 0
+let _1 = 0
+let X = 0
+let Y = 0
+_1 = 2
+index = 1
+_3 = 1
+_4 = 1
+_5 = 0
+_6 = 1
